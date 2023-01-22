46bc9fb44f20e13e171f3f99b6ddbcf32153c27f
Orson <55061548+0carb0n@users.noreply.github.com>
2023-01-22 13:43:15 -0500

    added auth router

--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const AuthController = require("../controllers/auth-controller");
 
 router.get("/login", AuthController.login);
-router.get("/callback", AuthController.callback);
+router.get("/login/callback", AuthController.loginCallback);
 
 module.exports = router;
