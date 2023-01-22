3eaea12fdd4530ec3f55cc144e98b8de32c577d7
Orson <55061548+0carb0n@users.noreply.github.com>
2023-01-22 13:50:05 -0500

    changed uri field for id field in user model

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -5,7 +5,8 @@ const UserSchema = new Schema(
     {
         displayName: { type: String, required: true },
         email: { type: String, required: true },
-        uri: { type: String, required: true },
+        id: { type: String, required: true },
+        refreshToken: { type: String, required: true },
     },
     { timestamps: true }
 );
