f25a291c88401be1fafd5b318fc35fc30d39005a
Orson <55061548+0carb0n@users.noreply.github.com>
2023-01-23 15:01:39 -0500

    finished server side

--- server/controllers/api-controller.js
+++ server/controllers/api-controller.js
@@ -10,10 +10,42 @@ const getQueue = async (req, res) => {
 
 		const queue = await api.getQueue(user);
 
-		res.status(200).json(queue)
+		res.status(200).json(queue);
+	})
+}
+
+const search = async (req, res) => {
+	User.findOne({ url: req.params.url}, async (err, user) => {
+		if (err || !req.query || !req.query.q) {
+			return res.status(400);
+		}
+
+		const results = await api.search(user, req.query.q);
+
+		res.status(200).json(results);
+	})
+}
+
+const addToQueue = async (req, res) => {
+	User.findOne({ url: req.params.url}, async (err, user) => {
+		if (err || !req.query || !req.query.id) {
+			return res.status(400);
+		}
+
+		const response = await api.addToQueue(user, req.query.id);
+
+		if (response) {
+			res.status(200);
+		} else {
+			res.status(400);
+		}
+
+		res.end();
 	})
 }
 
 module.exports = {
 	getQueue,
+	search,
+	addToQueue,
 }
