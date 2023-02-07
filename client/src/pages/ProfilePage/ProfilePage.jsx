f780ac47bea7f1c3e4fa777159bd69af9da37e0a
Orson Jiang <orsonjiang@gmail.com>
2023-02-07 11:00:01 -0500

    added profile page

---
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import api from "../../api/api";
+
+const ProfilePage = () => {
+	const [user, setUser] = useState("");
+
+	useEffect(() => {
+		const fetchUrl = async () => {
+			const response = await api.getProfile();
+			console.log(response.data.user)
+			setUser(response.data.user)
+		}
+
+		fetchUrl().catch(console.error)
+
+	}, [user.displayName, user.url])
+
+	const url = `http://localhost:5173/${user.url}`
+
+    return (
+        <div className="flex min-h-screen flex-col justify-center bg-neutral-50 text-center text-neutral-900 dark:bg-neutral-800 dark:text-neutral-50">
+            <div className="m-16 text-5xl">Spotify for Car</div>
+			<div>Welcome {user.displayName}</div>
+			<div>URL: <a href={url}>{url}</a></div>
+        </div>
+    );
+};
+
+export default ProfilePage;
