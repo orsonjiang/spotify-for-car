55c72dd51628447351decdde19070848f50c55c0
Orson Jiang <orsonjiang@gmail.com>
2023-04-30 00:25:14 -0400

    Added site wrapper

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -17,7 +17,7 @@ const ProfilePage = () => {
 
     if (!user) {
         return (
-            <div className="flex min-h-screen flex-col justify-center bg-neutral-50 text-center text-neutral-900 dark:bg-neutral-800 dark:text-neutral-50">
+            <div>
                 <div className="m-16 text-5xl">Spotify for Car</div>
                 <div className="px-32">Oh no! It seems like there was in issue authenticating you. The app is currently whitelist only so if you haven't contacted me at orsonjiang@gmail.com you probably don't have access. Feel free to reach out!</div>
             </div>
@@ -25,7 +25,7 @@ const ProfilePage = () => {
     }
 
     return (
-        <div className="flex min-h-screen flex-col justify-center bg-neutral-50 text-center text-neutral-900 dark:bg-neutral-800 dark:text-neutral-50">
+        <div>
             <div className="m-16 text-5xl">Spotify for Car</div>
             <div>Welcome {user.displayName}</div>
             <div className="px-32 break-all">
---
+++ client/src/pages/components/SiteWrapper.jsx
@@ -0,0 +1,11 @@
+const SiteWrapper = (props) => {
+    const { content } = props;
+
+    return (
+        <div className="flex min-h-screen flex-col justify-center bg-neutral-50 text-center text-neutral-900 dark:bg-neutral-800 dark:text-neutral-50">
+            {content}
+        </div>
+    );
+};
+
+export default SiteWrapper;
