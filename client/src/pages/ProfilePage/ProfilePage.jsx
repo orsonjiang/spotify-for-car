8277648b9b4344b397af3e0d2c8e08dab9ed4b82
Orson Jiang <orsonjiang@gmail.com>
2023-04-29 21:57:35 -0400

    ProfilePage typo

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -19,7 +19,7 @@ const ProfilePage = () => {
         return (
             <div className="flex min-h-screen flex-col justify-center bg-neutral-50 text-center text-neutral-900 dark:bg-neutral-800 dark:text-neutral-50">
                 <div className="m-16 text-5xl">Spotify for Car</div>
-                <div className="px-32">Oh no! It seems like there was in issue authenticating you. The app is currently whitelist only so if you have contacted me at orsonjiang@gmail.com you probably don't have access. Feel free to reach out!</div>
+                <div className="px-32">Oh no! It seems like there was in issue authenticating you. The app is currently whitelist only so if you haven't contacted me at orsonjiang@gmail.com you probably don't have access. Feel free to reach out!</div>
             </div>
         );
     }
