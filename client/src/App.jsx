13658807972131be3c5ebb9c508970657bcc34f8
Orson Jiang <orsonjiang@gmail.com>
2023-05-10 03:28:25 -0400

    Restructure app 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SiteWrapper from "./pages/components/SiteWrapper";
 
-import HomePage from "./pages/HomePage/HomePage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import RoomPage from "./pages/RoomPage/RoomPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
     return (
         <BrowserRouter>
             <SiteWrapper content={
                 <Routes>
-                    <Route exact path="/" element={<HomePage/>} />
-                    <Route exact path="/profile" element={<ProfilePage/>} />
-                    <Route exact path="/:roomId" element={<RoomPage/>} />
+                    <Route path="/*" element={<MainPage/>} />
                 </Routes>
             } />
         </BrowserRouter>
