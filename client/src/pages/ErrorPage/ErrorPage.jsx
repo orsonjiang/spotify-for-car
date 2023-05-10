cb314aafccf0a0ca43626f9c6c3181cc3117b3d2
Orson Jiang <orsonjiang@gmail.com>
2023-05-10 05:17:50 -0400

    Text changes

--- client/src/pages/ErrorPage/ErrorPage.jsx
+++ client/src/pages/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,16 @@
 const ErrorPage = () => {
     return (
-        <div className="flex-col">
+        <div className="">
             <div>
                 <h1 className="m-16 text-5xl">Spotify for Car</h1>
             </div>
-            <div className="px-32">
-                Oh no! It seems like there was in issue authenticating you. The
-                app is currently whitelist only so if you haven't contacted me
+            <div className="mx-10">
+                <span>Oh no! It seems like there was an issue authenticating you.</span>
+                <br />
+                <br />
+                <span> The app is currently whitelist only so if you haven't contacted me
                 at orsonjiang@gmail.com you probably don't have access. Feel
-                free to reach out!
+                free to reach out!</span>
             </div>
         </div>
     );
