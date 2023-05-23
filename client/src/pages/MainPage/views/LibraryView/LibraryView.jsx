23cea6743dc0622b149cbbf246cd1b19017eef28
Orson Jiang <orsonjiang@gmail.com>
2023-05-23 15:17:14 -0400

    Moved back to library button to bottom navbar

--- client/src/pages/MainPage/views/LibraryView/LibraryView.jsx
+++ client/src/pages/MainPage/views/LibraryView/LibraryView.jsx
@@ -1,12 +1,7 @@
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
-import {
-    setAddedSong,
-    setQueue,
-    setLibrary,
-    setPlaylist,
-} from '../../../../actions';
+import { setAddedSong, setQueue, setPlaylist } from '../../../../actions';
 import store from '../../../../store';
 import api from '../../../../api/api';
 import PlaylistCard from './components/PlaylistCard';
@@ -20,14 +15,12 @@ const LibraryView = () => {
 
     const handleSetPlaylist = async (id) => {
         const res = await api.getPlaylist(id);
-        store.dispatch(setPlaylist({
-            name: library.filter((playlist) => playlist.id == id)[0].name,
-            songs: res.data.items,
-        }));
-    };
-
-    const handleClearPlaylist = () => {
-        store.dispatch(setPlaylist([]));
+        store.dispatch(
+            setPlaylist({
+                name: library.filter((playlist) => playlist.id == id)[0].name,
+                songs: res.data,
+            })
+        );
     };
 
     const handleAddSong = async (trackId) => {
@@ -60,43 +53,17 @@ const LibraryView = () => {
         content = (
             <div className="flex-col">
                 <div>
-                    <div className="flex align-middle">
-                        <div className="absolute top-7 bottom-0 left-4">
-                            <button
-                                type="button"
-                                className="inline-flex items-center rounded-full border border-green-700 p-2.5 text-center text-sm font-medium text-green-700 hover:bg-green-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-300 dark:border-green-500 dark:text-green-500 dark:hover:bg-green-500 dark:hover:text-white dark:focus:ring-green-800"
-                                onClick={handleClearPlaylist}
-                            >
-                                <svg
-                                    className="h-5 w-5"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth={3}
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    aria-hidden="true"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-                                    />
-                                </svg>
-                                <span className="sr-only">
-                                    Icon description
-                                </span>
-                            </button>
-                        </div>
-                        <div className="my-8 mx-auto self-center text-3xl">
-                            {playlist.name}
-                        </div>
+                    <div className="my-8 mx-auto self-center text-3xl">
+                        {playlist.name}
                     </div>
                     <div className="m-auto max-w-lg">
                         <ul>
                             {playlist.songs.map((song) => {
                                 return (
                                     <SongCard
-                                        key={'queue-' + song.track.id + '-' + i++}
+                                        key={
+                                            'queue-' + song.track.id + '-' + i++
+                                        }
                                         song={song.track}
                                         onClick={handleAddSong}
                                     />
