5f5524ee64a37009d7d7adf999f23ba09ea9cc9a
Orson Jiang <orsonjiang@gmail.com>
2023-05-23 12:31:19 -0400

    Moved library data to main store

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,7 +1,7 @@
 import api from "../api/api";
 import auth from "../api/authApi";
 import store from "../store";
-import { setRoom, setQueue, setUser } from "../actions";
+import { setRoom, setQueue, setUser, setLibrary } from "../actions";
 
 export const fetchRoomDetails = async (roomId) => {
 	let details = await api.getRoom(roomId)
@@ -17,3 +17,8 @@ export const fetchUser = async () => {
 	const response = await auth.getProfile();
 	store.dispatch(setUser(response.data.user));
 };
+
+export const fetchLibrary = async () => {
+	const res = await api.getLibrary();
+	store.dispatch(setLibrary(res.data.items));
+}
