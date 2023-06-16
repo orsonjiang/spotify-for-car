9d6555734f0322000e38edd26733ab3e284de7b0
Orson Jiang <orsonjiang@gmail.com>
2023-06-16 19:51:08 -0400

    Created alert component

--- client/src/pages/MainPage/components/Alert.jsx
+++ client/src/pages/MainPage/components/Alert.jsx
@@ -53,22 +53,18 @@ const Alert = () => {
 
         case ERROR_VIEW:
             classNameStr += 'text-red-800 border-red-300 bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800';
-
             break;
 
         case SUCCESS_VIEW:
             classNameStr += 'text-green-800 border-green-300 bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800';
-
             break;
 
         case WARNING_VIEW:
             classNameStr += 'text-yellow-800 border-yellow-300 bg-yellow-50 dark:bg-gray-800 dark:text-yellow-400 dark:border-yellow-800';
-
             break;
 
         case MISC_VIEW:
             classNameStr += 'text-gray-800 border-gray-300 bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-800';
-
             break;
 
         default:
