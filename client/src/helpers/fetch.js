87dacd3c3aedbbd40b895574faec737bbb93726e
Orson Jiang <orsonjiang@gmail.com>
2023-06-16 20:11:20 -0400

    Added error handling for nonexistant rooms.

--- client/src/helpers/fetch.js
+++ client/src/helpers/fetch.js
@@ -2,12 +2,21 @@ import api from "../api/api";
 import auth from "../api/authApi";
 import store from "../store";
 import { setRoom, setQueue, setUser, setLibrary, setAlert, clearAlert } from "../actions";
-import { LOADING_VIEW } from "../constants/alertTypes";
+import { ERROR_VIEW } from "../constants/alertTypes";
 
 export const fetchRoomDetails = async (roomId) => {
-	let details = await api.getRoom(roomId)
-	store.dispatch(clearAlert())
-	store.dispatch(setRoom(details.data));
+	try {
+		let details = await api.getRoom(roomId)
+		store.dispatch(clearAlert())
+		store.dispatch(setRoom(details.data));
+	} catch {
+		store.dispatch(setAlert("Error Loading Room", "The room does not exist. Try visiting another room.", ERROR_VIEW));
+		store.dispatch(setRoom({
+			picture_url: null,
+			displayName: null,
+			owner_id: null,
+		}))
+	}
 };
 
 export const fetchQueue = async (roomId) => {
