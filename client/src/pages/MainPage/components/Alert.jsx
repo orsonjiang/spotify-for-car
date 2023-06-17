934bd10986b690562283ad95c7dbb92d120b4aa0
Orson Jiang <orsonjiang@gmail.com>
2023-06-16 20:10:58 -0400

    Fixed imported icon for alert

--- client/src/pages/MainPage/components/Alert.jsx
+++ client/src/pages/MainPage/components/Alert.jsx
@@ -15,7 +15,7 @@ const Alert = () => {
     }
 
     let classNameStr = 'max-w-lg mx-auto absolute inset-x-1 top-1 z-10 flex p-4 text-sm border rounded-lg ';
-    let icon = (<svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>);
+    let icon = (<svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>);
 
     switch (type) {
         case LOADING_VIEW:
