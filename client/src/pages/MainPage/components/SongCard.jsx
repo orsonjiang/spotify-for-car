2a8a484c6647b336c49321bb2542fb5113ce3cfa
Orson Jiang <orsonjiang@gmail.com>
2023-06-22 17:11:54 -0400

    Changed song card title overflow styling

--- client/src/pages/MainPage/components/SongCard.jsx
+++ client/src/pages/MainPage/components/SongCard.jsx
@@ -6,7 +6,7 @@ const SongCard = (props) => {
     const { song, onClick } = props;
     const addedSong = useSelector((state) => state.search.addedSong);
 
-    let className = 'flex bg-zinc-200 dark:bg-zinc-900 rounded-xl m-2';
+    let className = 'flex bg-zinc-200 dark:bg-zinc-900 rounded-xl m-2 overflow-clip';
     if (song.id == addedSong) {
         className += ' border border-blue-600';
     }
@@ -18,8 +18,8 @@ const SongCard = (props) => {
             onClick={onClick ? () => onClick(song.id) : undefined}
         >
 			<AlbumCover album={song.album}/>
-            <div className="mt-2 flex flex-col text-left">
-                <p className="text-base font-normal">{song.name}</p>
+            <div className="mt-2 flex flex-col text-left mr-2 overflow-clip">
+                <p className="text-base font-normal truncate">{song.name}</p>
                 <p className="text-sm font-light">
                     {song.album.artists.map((artist) => artist.name).join(', ')}
                 </p>
