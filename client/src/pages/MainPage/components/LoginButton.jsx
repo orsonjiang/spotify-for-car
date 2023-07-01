42ae08f05e027876e7282ac7d7328d1ddcedb92a
Orson Jiang <orsonjiang@gmail.com>
2023-06-30 21:21:26 -0400

    Added state parameter and redirect after login

--- client/src/pages/MainPage/components/LoginButton.jsx
+++ client/src/pages/MainPage/components/LoginButton.jsx
@@ -1,6 +1,12 @@
+import qs from 'query-string';
+import { useLocation } from 'react-router-dom';
+
 const LoginButton = () => {
+    const location = useLocation();
+    const queryString = qs.stringify({location: location.pathname});
+    
     return (
-        <a href={`${import.meta.env.VITE_SERVER_URL}/auth/login`}>
+        <a href={`${import.meta.env.VITE_SERVER_URL}/auth/login?${queryString}`}>
             <button className="rounded-full bg-green-600 px-6 py-3.5 text-white duration-150 hover:bg-green-500 active:bg-green-700">
                 Login
             </button>
