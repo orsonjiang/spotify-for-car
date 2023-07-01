1838c3ef95410eff19b14af856453261b954a943
Orson Jiang <orsonjiang@gmail.com>
2023-07-01 12:24:07 -0400

    Added default duration for runAlert

--- client/src/helpers/alert.js
+++ client/src/helpers/alert.js
@@ -3,6 +3,8 @@ import { setAlert, clearAlert } from "../actions";
 import { sleep } from "./utils";
 
 export const runAlert = async (title, message, type, duration) => {
+	if (!duration) duration = 4000;
+
 	store.dispatch(setAlert(title, message, type));
 	await sleep(duration)
 	store.dispatch(clearAlert());
