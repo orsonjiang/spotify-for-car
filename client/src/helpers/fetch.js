10e186fc62430506539b96757a3daf2e076b0455
Orson Jiang <orsonjiang@gmail.com>
2023-08-01 14:44:27 -0400

    Caught errors when fetching from server

--- client/src/helpers/fetch.js
+++ client/src/helpers/fetch.js
@@ -3,11 +3,12 @@ import auth from "../api/authApi";
 import store from "../store";
 import { setRoom, setQueue, setUser, setLibrary, setAlert, clearAlert } from "../actions";
 import { ERROR_VIEW } from "../constants/alertTypes";
+import { runAlert } from "./alert";
 
 export const fetchRoomDetails = async (roomId) => {
 	try {
-		let details = await api.getRoom(roomId)
-		store.dispatch(clearAlert())
+		let details = await api.getRoom(roomId);
+		store.dispatch(clearAlert());
 		store.dispatch(setRoom(details.data));
 	} catch {
 		store.dispatch(setAlert("Error Loading Room", "The room does not exist. Try visiting another room.", ERROR_VIEW));
@@ -20,16 +21,34 @@ export const fetchRoomDetails = async (roomId) => {
 };
 
 export const fetchQueue = async (roomId) => {
-	let queue = await api.getQueue(roomId);
-	store.dispatch(setQueue(queue.data));
+	try {
+		let queue = await api.getQueue(roomId);
+		store.dispatch(setQueue(queue.data));
+	} catch (err) {
+		if (!err.response) {
+			runAlert("Connection Issue", "There is an issue with our servers. Please sit tight while we rectify the problem.", ERROR_VIEW);
+		}
+	}
 };
 
 export const fetchUser = async () => {
-	const response = await auth.getProfile();
-	store.dispatch(setUser(response.data.user));
+	try {
+		const res = await auth.getProfile();
+		store.dispatch(setUser(res.data.user));
+	} catch (err) {
+		if (!err.response) {
+			runAlert("Connection Issue", "There is an issue with our servers. Please sit tight while we rectify the problem.", ERROR_VIEW);
+		}
+	}
 };
 
 export const fetchLibrary = async () => {
-	const res = await auth.getLibrary();
-	store.dispatch(setLibrary(res.data.items));
+	try {
+		const res = await auth.getLibrary();
+		store.dispatch(setLibrary(res.data.items));
+	} catch (err) {
+		if (!err.response) {
+			runAlert("Connection Issue", "There is an issue with our servers. Please sit tight while we rectify the problem.", ERROR_VIEW);
+		}
+	}
 }
